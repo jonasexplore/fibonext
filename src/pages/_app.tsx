2bafc5680c6d659a1bab43d41b01fe784cf461ac
Jonas <jonasexplore@gmail.com>
2022-05-27 20:06:05 -0300

    feat(pages): added components

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,13 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import { GameProvider } from "../contexts/game";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <GameProvider>
+      <Component {...pageProps} />
+    </GameProvider>
+  );
 }
 
 export default MyApp;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,19 @@
 import type { NextPage } from "next";
+import { useContext } from "react";
+
+import { CardList } from "../components/CardList";
+import { GameContext } from "../contexts/game";
+
+import styles from "../styles/home.module.css";
 
 const Home: NextPage = () => {
-  return <div>Hello World</div>;
+  const { cards } = useContext(GameContext);
+
+  return (
+    <div className={styles.container}>
+      <CardList cards={cards} />
+    </div>
+  );
 };
 
 export default Home;

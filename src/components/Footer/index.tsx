1503632bb7613a3a48ca453810892d8504140001
Jonas <jonasexplore@gmail.com>
2022-05-28 15:18:44 -0300

    feat(components): created footer

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,8 @@
+import { Text } from "../Text";
+import styles from "./footer.module.css";
+
+export const Footer = () => (
+  <div className={styles.footer}>
+    <Text>Todos os direitos reservados.</Text>
+  </div>
+);

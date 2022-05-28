8283fe176dcb4ebc19a46d31e5e966dcea1f7046
Jonas <jonasexplore@gmail.com>
2022-05-28 15:20:15 -0300

    refactor(pages): remove unused components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,61 +1,46 @@
 import type { NextPage } from "next";
-import { useContext } from "react";
-import { Button, CardList, Text, User } from "../components";
-
-import { GameContext } from "../contexts/game";
 
 import styles from "../styles/home.module.css";
 
-import CreateIcon from "../assets/create.svg";
+import { Button } from "../components";
 import EnterIcon from "../assets/enter.svg";
+import CreateIcon from "../assets/create.svg";
+import { generateId } from "../utils";
 
 const Home: NextPage = () => {
-  const { cards } = useContext(GameContext);
-
   return (
-    <>
-      <div className={styles.header}>
-        <Text>Fibonext</Text>
+    <div className={styles.container}>
+      <div className={styles.groupButtons}>
+        <Button
+          large
+          icon={
+            <CreateIcon
+              fill="#f8f8f2"
+              width={24}
+              height={24}
+              alt="create icon"
+            />
+          }
+          href={`/room/${generateId(7)}`}
+        >
+          Criar sala
+        </Button>
+        <Button
+          large
+          icon={
+            <EnterIcon
+              fill="#f8f8f2"
+              width={24}
+              height={24}
+              alt="create icon"
+            />
+          }
+          href={`/connect`}
+        >
+          Entrar
+        </Button>
       </div>
-      <div className={styles.container}>
-        <div className={styles.groupButtons}>
-          <Button
-            large
-            icon={
-              <CreateIcon
-                fill="#f8f8f2"
-                width={24}
-                height={24}
-                alt="create icon"
-              />
-            }
-          >
-            Criar sala
-          </Button>
-          <Button
-            large
-            icon={
-              <EnterIcon
-                fill="#f8f8f2"
-                width={24}
-                height={24}
-                alt="create icon"
-              />
-            }
-          >
-            Entrar
-          </Button>
-        </div>
-        <Button>Resetar</Button>
-        <Button>Revelar</Button>
-        <User />
-        <Text>Escolha uma das opções abaixo:</Text>
-        <CardList cards={cards} />
-      </div>
-      <div className={styles.footer}>
-        <Text>Todos os direitos reservados.</Text>
-      </div>
-    </>
+    </div>
   );
 };
 

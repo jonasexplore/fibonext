20bf110332f16db4e3af6a9cefa0792b0a431e3c
Jonas <jonasexplore@gmail.com>
2022-05-28 15:19:15 -0300

    chore: exported all components

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -3,3 +3,6 @@ export * from "./CardList";
 export * from "./Text";
 export * from "./User";
 export * from "./Button";
+export * from "./Header";
+export * from "./Footer";
+export * from "./Input";

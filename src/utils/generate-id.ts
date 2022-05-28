0de466cf26fa9dea1ecc359af13d0f1bd4c3688f
Jonas <jonasexplore@gmail.com>
2022-05-28 15:20:39 -0300

    feat(utils): added function to create room id

---
+++ src/utils/generate-id.ts
@@ -0,0 +1,14 @@
+export function generateId(length: Number) {
+  let result = "";
+
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+  const charactersLength = characters.length;
+
+  for (var i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+
+  return result;
+}

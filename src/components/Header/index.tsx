a18de24434a7179a80326fad1957dee4ed20903a
Jonas <jonasexplore@gmail.com>
2022-05-28 15:18:53 -0300

    feat(components): created header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,10 @@
+import { Text } from "../Text";
+import styles from "./header.module.css";
+
+export const Header = () => {
+  return (
+    <div className={styles.header}>
+      <Text>Fibonext</Text>
+    </div>
+  );
+};

d25cb13c1514e18777cb4463d7a8321cffc21874
Jonas <jonasexplore@gmail.com>
2022-05-28 15:19:39 -0300

    feat(pages): created connect

---
+++ src/pages/connect.tsx
@@ -0,0 +1,42 @@
+import type { NextPage } from "next";
+import { useRouter } from "next/router";
+import { FormEvent } from "react";
+import { Button, Input } from "../components";
+
+import styles from "../styles/connect.module.css";
+
+const field = {
+  name: "roomId",
+  label: "Informe o código da sala",
+};
+
+const Connect: NextPage = () => {
+  const router = useRouter();
+
+  const onSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const form = event.target as HTMLFormElement;
+
+    router.push(`/room/${form[field.name]?.value}`);
+  };
+
+  return (
+    <form onSubmit={onSubmit}>
+      <div className={styles.container}>
+        <Input
+          label={field.label}
+          minLength={7}
+          maxLength={7}
+          required
+          name={field.name}
+        />
+        <div className={styles.button}>
+          <Button type="submit">Entrar</Button>
+        </div>
+      </div>
+    </form>
+  );
+};
+
+export default Connect;

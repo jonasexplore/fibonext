0da02a5484839d7ffce71c941ebe01f1e513de04
Jonas <jonasexplore@gmail.com>
2022-05-28 15:19:59 -0300

    refactor(pages): added header and footer

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,15 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+
 import { GameProvider } from "../contexts/game";
+import { Footer, Header } from "../components";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <GameProvider>
+      <Header />
       <Component {...pageProps} />
+      <Footer />
     </GameProvider>
   );
 }
